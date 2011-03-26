80673661084976fbbfcad9c44689fc1576cd6eb7
Greg Braman <bramanga@gmail.com>
2011-03-26 16:18:20 -0400

    Created initial application and generated cucumber scripts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DocManager::Application.config.secret_token = '0ec68f7e0c989c69bc49fdb28bf80f9a4b28f11a051ed3aff295af382bb8e3daf3f61bcd24ccf383889174a042d6d03866b97a13ca738567716abf1f4eee0cc1'
